fa6988f87601f354629f98dbebd04c393e0b5bdd
Maximiliano Valli <127133282+maxivalli@users.noreply.github.com>
2026-02-04 02:08:30 -0300

    fix icons

--- client/vite.config.js
+++ client/vite.config.js
@@ -17,12 +17,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/icon-192x192.png',
+            src: '/icon-192-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icon-512x512.png',
+            src: '/icon-512-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
